1ee57fa800f240adc4a45d471438d023c28790a9
Parrual K <kinopio500@gmail.com>
2021-06-23 09:26:18 +0900

    Add files via upload

--- easy_tac.sh
+++ easy_tac.sh
@@ -5,3 +5,33 @@ source $DEV_UTIL/include.sh
 
 # main
 cyan_echo "===== Script [easy_tac.sh] is executed. ====="
+
+if [ "$TB_HOME" != `echo \`pwd\`` ]; then
+    echo "Please check \$TB_HOME and current path."
+    echo "TB_HOME: $TB_HOME"
+else
+    {   # generate tip files with UTF-8
+        cyan_echo "STEP 0: Get neo-gen scripts"
+        echo `pwd` 
+        cp ~/tibero_Backup/gen_tip_for_tac.sh $TB_HOME/config
+        cp ~/tibero_Backup/tbctl $TB_HOME/bin
+    }
+    {
+        cd $TB_HOME
+        cyan_echo "STEP 1: Make Tip Files."
+        cd config
+        rm cm.*.tip
+        rm tibero*.tip
+        rm psm_commands
+        rm $TB_HOME/client/config/tbdsn.tbr
+
+        ./gen_tip_for_tac.sh -n 2 -s tibero
+    }
+    {
+        cd $TB_HOME
+        cyan_echo "STEP 2: New Mount."
+        tb_newmount.sh -n 2 -s tibero
+    }
+fi
+
+
--- shm_free.sh
+++ shm_free.sh
@@ -5,3 +5,10 @@ source $DEV_UTIL/include.sh
 
 # main
 cyan_echo "===== Script [shm_free.sh] is executed. ====="
+
+cmd="$(ipcs -m | grep $1 | awk '{ print $2 }')"
+if [ -n cmd ]; then 
+    cyan_echo "Try to Remove Shared Memory: $cmd"
+    ipcrm -m $cmd
+    cyan_echo "Complete to Remove Shared Memory: $cmd" 
+fi
